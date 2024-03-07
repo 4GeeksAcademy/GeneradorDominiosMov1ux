fdb31cb3ae2363e52c126a2ec3fcb8733c88c191
Mov1ux <104890041+Mov1ux@users.noreply.github.com>
2024-03-07 15:29:01 +0000

    third commit

--- src/app.js
+++ src/app.js
@@ -5,22 +5,25 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-let name = [
-  "tech",
-  "4weeks",
-  "mov1ux",
-  "array",
-  "loops",
-  "html",
-  "python",
-  "JS"
-];
-let ext = [".com", ".es", ".info", ".org", ".dev"];
-export function generate() {
+function generar() {
+  let name = [
+    "tech",
+    "4weeks",
+    "mov1ux",
+    "array",
+    "loops",
+    "html",
+    "python",
+    "JS"
+  ];
+  let ext = [".com", ".es", ".info", ".org", ".dev"];
+
   let generateName = name[Math.floor(Math.random() * name.length)];
   let generateExt = ext[Math.floor(Math.random() * ext.length)];
-  let doma;
-  return generateName + generateExt;
+  let domainName = generateName + generateExt;
+  document.getElementById("DomainName").textContent =
+    "Nombre del dominio generado: " + domainName;
+  //return generateName + generateExt;
 }
-let generateDomain = generate();
+let generateDomain = generar();
 console.log("Nombre dominio:", generateDomain);
